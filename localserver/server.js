8d91110f499201448f01c5a555fb8a499508255f
gsauarv <gsauarv>
2020-10-25 15:53:48 +0545

    Path Routing and HTML Views

---
+++ localserver/server.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs'); 
+const server = http.createServer((req,res)=>
+{
+    console.log(req.url , req.method);
+    // set header content type
+    res.setHeader('Content-Type','text/html');
+    let path = './views/';
+    switch(req.url)
+    {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        
+        default:
+            path += '404.html';
+            break;
+    }
+    // Send HTML files
+
+    fs.readFile(path,(err,data)=>
+    {
+        if(err)
+        {
+            console.log(err);
+            res.end();
+        }
+        else
+        {
+            res.end(data);
+        }
+  
+    });
+
+});
+
+server.listen(3000,'localhost',()=>
+{
+    console.log("Listing For Request for 3000");
+});
+
+
+
